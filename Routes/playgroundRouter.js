cb48442e8cb5b8c4fdca2e44dc6046774b3cd22b
MohamedMF2 <furious_fouad@yahoo.com>
2020-02-11 10:51:43 +0200

    add playground model and route

---
+++ Routes/playgroundRouter.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+const playgroundRouter = require("express").Router();
+const verify = require("../Validations/verifyToken");
+
+require("../Models/Playground");
+
+let playgroundSchema = mongoose.model("playgrounds");
+
+
+
+playgroundRouter.route('/playground/:_id?')
+
+.get((request, response) => {
+    if(request.params._id){
+        playgroundSchema.find({_id:request.params._id})
+    .then((result) => {
+        response.send(result);
+    })
+    .catch((error) => { response.send(error) })
+    }else{
+    playgroundSchema.find({})
+    .then((result) => {
+        response.send(result);
+    })
+    .catch((error) => { response.send(error) })
+    }
+})  //end
+
+
+.post((request, response) => {
+    let playgroundObject = new playgroundSchema({
+        _id:request.body._id,
+        name:request.body.name,
+        image:request.body.image,
+        address:request.body.address,
+        location:request.body.location,
+        dayPrice:request.body.dayPrice,
+        nightPrice:request.body.nightPrice,
+        covered:request.body.covered,
+        category_id:request.body.category_id,
+        owner_id:request.body.owner_id,
+        details:request.body.details,
+
+    });
+    playgroundObject.save().then((result) => {
+        response.send(result);
+    }).catch((error) => {
+        response.send(error);
+    })
+}) //end of post
+
+
+
+.put((request, response) => {
+    playgroundSchema.updateOne({ _id: request.body._id }, {
+        $set: {
+            name:request.body.name,
+            image:request.body.image,
+            address:request.body.address,
+            location:request.body.location,
+            dayPrice:request.body.dayPrice,
+            nightPrice:request.body.nightPrice,
+            covered:request.body.covered,
+            category_id:request.body.category_id,
+            owner_id:request.body.owner_id,
+            details:request.body.details,
+        }
+    }).then((result) => {
+        response.send(result);
+    }).catch((error) => {
+        response.send(error);
+    })
+})//end of put
+
+
+  .delete((request, response) => {
+    playgroundSchema.deleteOne({ _id: request.body._id })
+    .then((result) => { response.send(result) })
+    .catch(error => { response.send(error) })
+});//end off delete 
+module.exports = playgroundRouter
